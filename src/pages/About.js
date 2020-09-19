94717920916eee1ea515cd3324e4eeb9a0581570
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-09-19 20:18:27 +0200

    Add styling for About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-//import '../styles/pages/About.scss';
+import '../styles/pages/About.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faHtml5,
