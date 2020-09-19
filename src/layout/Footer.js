d247f66fbba93e419cca04f2f8171d988446a976
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-09-19 19:55:32 +0200

    Add styling for Footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import '../styles/layout/Footer.scss';
+import '../styles/layout/Footer.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
