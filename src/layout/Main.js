940dcf37d70d440e85458ab8231693e8c44837f0
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-09-19 20:11:16 +0200

    Add Main.js and Error.js Create structure of About, Portfolio, Contact

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
+//import { Container, Row, Col } from 'react-bootstrap';
+import Portfolio from '../pages/Portfolio';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Error from '../pages/Error';
+import '../styles/layout/Main.scss';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
-  }
+  state = {}
   render() {
     return (
       <Switch>
-
-      </Switch> );
+        <Route path='/' exact component={About} />
+        <Route path='/portfolio' component={Portfolio} />
+        <Route path='/contact' component={Contact} />
+        <Route component={Error} />
+      </Switch>
+    );
   }
 }
 
