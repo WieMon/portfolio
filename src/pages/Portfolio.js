3183cc233d8b98382fd935bbffc892bd6f6de852
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-10-07 21:17:51 +0200

    Add responsive images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../styles/pages/Portfolio.scss';
-import portfoliotest from '../images/admin_560.png';
-import admin from '../images/admin_800.png';
-import todolist from '../images/todolist_800.png';
-import rps from '../images/rps_800.png';
+import portfoliotest800 from '../images/admin_800.png';
+import portfoliotest560 from '../images/admin_560.png';
+import admin800 from '../images/admin_800.png';
+import admin560 from '../images/admin_560.png';
+import todolist800 from '../images/todolist_800.png';
+import todolist560 from '../images/todolist_560.png';
+import rps800 from '../images/rps_800.png';
+import rps560 from '../images/rps_560.png';
 
 const Portfolio = () => {
   return (
@@ -14,7 +18,10 @@ const Portfolio = () => {
           <Col sm={12} md={6} className='p-2 portfolio__col'>
             <div className='portfolio__wrapper'>
               <div className='portfolio__img-div'>
-                <img src={portfoliotest} alt='' className='portfolio__img'></img>
+                <img srcSet={`${portfoliotest560} 1450w,
+                              ${portfoliotest800} 1600w`}
+                src={portfoliotest560}
+                alt='' className='portfolio__img'></img>
               </div>
               <div className='portfolio__description-div'>
                 <h5 className='portfolio__title'>
@@ -48,7 +55,10 @@ const Portfolio = () => {
           <Col sm={12} md={6} className='p-2 portfolio__col'>
             <div className='portfolio__wrapper'>
               <div className='portfolio__img-div'>
-                <img src={admin} alt='' className='portfolio__img'></img>
+                <img srcSet={`${admin560} 1450w,
+                              ${admin800} 1600w`}
+                src={admin560}
+                alt='' className='portfolio__img'></img>
               </div>
               <div className='portfolio__description-div'>
                 <h5 className='portfolio__title'>
@@ -84,7 +94,10 @@ const Portfolio = () => {
           <Col sm={12} md={6} className='p-2 portfolio__col'>
             <div className='portfolio__wrapper'>
               <div className='portfolio__img-div'>
-                <img src={todolist} alt='To-Do List' className='portfolio__img'></img>
+                <img  srcSet={`${todolist560} 1450w,
+                              ${todolist800} 1600w`}
+                src={todolist560}
+                alt='To-Do List' className='portfolio__img'></img>
               </div>
               <div className='portfolio__description-div'>
                 <h5>
@@ -118,7 +131,10 @@ const Portfolio = () => {
           <Col sm={12} md={6} className='p-2 portfolio__col'>
             <div className='portfolio__wrapper'>
               <div className='portfolio__img-div'>
-                <img src={rps} alt='Rock, paper, scissors' className='portfolio__img'></img>
+                <img srcSet={`${rps560} 1450w,
+                              ${rps800} 1600w`}
+                src={rps560}
+                alt='Rock, paper, scissors' className='portfolio__img'></img>
               </div>
               <div className='portfolio__description-div'>
                 <h5 className='portfolio__title'>
