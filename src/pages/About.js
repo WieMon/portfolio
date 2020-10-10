5463c7a8266087f31f2fe7bffbe01c564ceb8f44
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-10-10 20:10:26 +0200

    Fix classes naming for About

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,96 +21,96 @@ const About = () => {
   return (
     <div className='about'>
       <Container className='p-4'>
-        <Row className='p-4 about__text-row'>
-          <Col className='p-2'>
+        <Row className='p-4 about__row about__row--text'>
+          <Col sm={12} className='p-2'>
             <p >
-              <span className='about__text-span'>Welcome</span>
+              <span className='about__bold'>Welcome</span>
             </p>
             <p>
               My name is Monika and I am looking for a position of junior front-end developer.
             </p>
             <p>
-              My first experience with programming and visualization of complex data was during my PhD. I was using statistical and graphical packages of <span className='about__text-span'>R</span> and <span className='about__text-span'>CorelDraw</span> to show large datasets as audience-friendly presentations.
+              My first experience with programming and visualization of complex data was during my PhD. I was using statistical and graphical packages of <span className='about__bold'>R</span> and <span className='about__bold'>CorelDraw</span> to show large datasets as audience-friendly presentations.
             </p>
             <p>
-              Then, I have started working in fast-paced environment of large corporations. Ever since then effective multitasking and cross-cultural communication in <span className='about__text-span'>English</span> or, occasionally, in <span className='about__text-span'>German</span> have become my daily routine.
+              Then, I have started working in fast-paced environment of large corporations. Ever since then effective multitasking and cross-cultural communication in <span className='about__bold'>English</span> or, occasionally, in <span className='about__bold'>German</span> have become my daily routine.
             </p>
             <p>
               However, I am passionate about learning new things and have been missing tasks requiring analytical thinking and problem solving.
             </p>
             <p>
-              Therefore, at the beginning of 2019 I have started learning web development through a several-month long bootcamp (<span className='about__text-span'>
-              Kodilla</span>), the online and face-to-face courses (<span className='about__text-span'>Udemy</span>, <span className='about__text-span'>Stacja IT</span>), and on my own.
+              Therefore, at the beginning of 2019 I have started learning web development through a several-month long bootcamp (<span className='about__bold'>
+              Kodilla</span>), the online and face-to-face courses (<span className='about__bold'>Udemy</span>, <span className='about__bold'>Stacja IT</span>), and on my own.
             </p>
           </Col>
         </Row>
         <Row>
-          <Col xl={12}>
-            <p className='about__text-skills'>
-              <span className='about__text-skills-span'>My skills so far ...</span>
-            </p>
+          <Col xl={12} className='about__title-col'>
+            <h5 className='about__title'>
+            My skills so far ...
+            </h5>
           </Col>
         </Row>
-        <Row className='p-0 about__skills-row'>
+        <Row className='p-0 about__row about__row--skills'>
           <Col xl={12}>
-            <p className='about__skills'>
+            <h5 className='about__subtitle'>
             Languages and libraries
-            </p>
+            </h5>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faHtml5} className='about__icon' />
-            <span className='about__icon-span'>HTML</span>
+            <span className='about__icon-name'>HTML</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faCss3Alt} className='about__icon' />
-            <span className='about__icon-span'>CSS</span>
+            <span className='about__icon-name'>CSS</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faSass} className='about__icon' />
-            <span className='about__icon-span'>Sass</span>
+            <span className='about__icon-name'>Sass</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faJs} className='about__icon' />
-            <span className='about__icon-span'>JavaScript</span>
+            <span className='about__icon-name'>JavaScript</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faReact} className='about__icon' />
-            <span className='about__icon-span'>React</span>
+            <span className='about__icon-name'>React</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faBootstrap} className='about__icon' />
-            <span className='about__icon-span'>Bootstrap</span>
+            <span className='about__icon-name'>Bootstrap</span>
           </Col>
         </Row>
-        <Row className='p-0 about__skills-row'>
+        <Row className='p-0 about__row about__row--skills'>
           <Col xl={12}>
-            <p className='about__skills'>
+            <h5 className='about__subtitle'>
             Tools
-            </p>
+            </h5>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faGitAlt} className='about__icon' />
-            <span className='about__icon-span'>Git</span>
+            <span className='about__icon-name'>Git</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faNpm} className='about__icon' />
-            <span className='about__icon-span'>npm</span>
+            <span className='about__icon-name'>npm</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faYarn} className='about__icon' />
-            <span className='about__icon-span'>Yarn</span>
+            <span className='about__icon-name'>Yarn</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faJira} className='about__icon' />
-            <span className='about__icon-span'>Jira</span>
+            <span className='about__icon-name'>Jira</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
             <FontAwesomeIcon icon={faSlack} className='about__icon' />
-            <span className='about__icon-span'>Slack</span>
+            <span className='about__icon-name'>Slack</span>
           </Col>
-          <Col sm={12} md={4} className='p-4 about__skills-col'>
-            <img src={corel} alt='Corel logo' className='about__icon' />
-            <span className='about__icon-span'>CorelDRAW, Inkscape</span>
+          <Col sm={12} md={4} className='p-4 about__icon-col'>
+            <img src={corel} alt='Corel logo' className='about__icon about__icon--img' />
+            <span className='about__icon-name'>CorelDRAW, Inkscape</span>
           </Col>
         </Row>
       </Container>
