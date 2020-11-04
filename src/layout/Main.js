5a7165255bd9401bf33d85f7324446d4396d199a
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-11-04 18:30:31 +0100

    Fix homepage in package.json

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,5 +1,5 @@
 import React, { Component, lazy, Suspense } from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import About from '../pages/About';
 import '../styles/layout/Main.scss';
 
