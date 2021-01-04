fff0b1449cbbec19fa4230b6f37ca4bfe09505f5
Monika Wieczorek <monikawieczoreq@gmail.com>
2021-01-04 15:43:59 +0100

    Add skills to About Fix description in About

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../styles/pages/About.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileImage } from '@fortawesome/free-solid-svg-icons';
+import { faFileImage, faServer } from '@fortawesome/free-solid-svg-icons';
 import {
+  faReact,
+  faVuejs,
+  faJs,
   faHtml5,
   faCss3Alt,
   faSass,
-  faJs,
-  faReact,
   faBootstrap,
   faGitAlt,
   faNpm,
   faYarn,
   faJira,
   faSlack,
+  faRProject,
 } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
@@ -32,17 +34,17 @@ const About = () => {
               My name is Monika and I am looking for a position of <span className='about__bold'>junior front-end developer</span>.
             </p>
             <p>
-              Since 2019 I have been learning the web development through a several-month long bootcamp <span className='about__bold'>(Kodilla Web Developer)</span>, the online and face-to-face courses <span className='about__bold'>(Udemy, Stacja IT)</span>, and on my own.
-            </p>
-            <p>
-              Before that, I had modelled in <span className='about__bold'>the R language for statistical computing </span>during my PhD project.
+              My first experience with programming was during my PhD when I was building statistical models using <span className='about__bold'>the R language for statistical computing </span> to analyse data.
               I also used graphical packages of <span className='about__bold'>R</span> and <span className='about__bold'>CorelDraw</span> to present the complex data in audience-friendly ways.
             </p>
             <p>
-              After my PhD, I worked in fast-paced environment of large corporations. Ever since then effective multitasking and cross-cultural communication in <span className='about__bold'>English</span> and, occasionally, in <span className='about__bold'>German</span> became my daily routine.
+              Then I worked in fast-paced environment of large corporations. Ever since then effective multitasking and cross-cultural communication in <span className='about__bold'>English</span> and occasionally in <span className='about__bold'>German</span> became my daily routine.
             </p>
             <p>
-              However, I am passionate about learning, analyzing and solving problems. Thus, I decided to look for another possibility for my job that would allow me to combine my desire to continuous development and the experience I have got so far.
+              However, due to my passion for learning and constant curiosity my interest in programming began to grow... Therefore, in 2019 I have started learning the web development through a several-month long bootcamp <span className='about__bold'>(Kodilla Web Developer)</span>, the online and face-to-face courses <span className='about__bold'>(Udemy, Stacja IT)</span>, and on my own.
+            </p>
+            <p>
+              If you hire me, you will gain my analyzing and problem solving skills, desire to continuous learning and experience I have got so far.
             </p>
           </Col>
         </Row>
@@ -57,6 +59,10 @@ const About = () => {
           <Col sm={12} md={4} className='about__icon-col'>
             <FontAwesomeIcon icon={faReact} className='about__icon' />
             <span className='about__icon-name'>React</span>
+          </Col>
+          <Col sm={12} md={4} className='about__icon-col'>
+            <FontAwesomeIcon icon={faVuejs} className='about__icon' />
+            <span className='about__icon-name'>Vue.js</span>
           </Col>
           <Col sm={12} md={4} className='about__icon-col'>
             <FontAwesomeIcon icon={faJs} className='about__icon' />
@@ -77,6 +83,10 @@ const About = () => {
           <Col sm={12} md={4} className='about__icon-col'>
             <FontAwesomeIcon icon={faBootstrap} className='about__icon' />
             <span className='about__icon-name'>Bootstrap</span>
+          </Col>
+          <Col sm={12} md={4} className='about__icon-col'>
+            <FontAwesomeIcon icon={faServer} className='about__icon' />
+            <span className='about__icon-name'>Firebase</span>
           </Col>
           <Col sm={12} md={4} className='about__icon-col'>
             <FontAwesomeIcon icon={faGitAlt} className='about__icon' />
@@ -101,6 +111,10 @@ const About = () => {
           <Col sm={12} md={4} className='about__icon-col'>
             <FontAwesomeIcon icon={faFileImage} className='about__icon' />
             <span className='about__icon-name'>CorelDRAW, Inkscape</span>
+          </Col>
+          <Col sm={12} md={4} className='about__icon-col'>
+            <FontAwesomeIcon icon={faRProject} className='about__icon' />
+            <span className='about__icon-name'>R for statistical computing</span>
           </Col>
         </Row>
       </Container>
