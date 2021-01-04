27be81ef501f0cea26f9b7870657784347cbb70f
Monika Wieczorek <monikawieczoreq@gmail.com>
2021-01-04 15:55:46 +0100

    Updates

---
+++ precache-manifest.9b32dc18a416f2e03c01c9bdec270c95.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4120377fc56d073b8c7089edfc3fcc3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4d85d656bc45e27db221",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3a31f1a31a05bc98a616",
+    "url": "/portfolio/static/css/3.3281c25d.chunk.css"
+  },
+  {
+    "revision": "352cbe09ccd406423b46",
+    "url": "/portfolio/static/css/4.dae56f71.chunk.css"
+  },
+  {
+    "revision": "014554a146fbe7062a94",
+    "url": "/portfolio/static/css/main.7068edb1.chunk.css"
+  },
+  {
+    "revision": "4d85d656bc45e27db221",
+    "url": "/portfolio/static/js/2.c22bed44.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.c22bed44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a31f1a31a05bc98a616",
+    "url": "/portfolio/static/js/3.14b5dd20.chunk.js"
+  },
+  {
+    "revision": "352cbe09ccd406423b46",
+    "url": "/portfolio/static/js/4.200acff8.chunk.js"
+  },
+  {
+    "revision": "014554a146fbe7062a94",
+    "url": "/portfolio/static/js/main.2dd97af3.chunk.js"
+  },
+  {
+    "revision": "70f32c60650f35b3ab75",
+    "url": "/portfolio/static/js/runtime-main.a8111abf.js"
+  },
+  {
+    "revision": "101c4a6253be98d7a981a93036b74c31",
+    "url": "/portfolio/static/media/admin.101c4a62.png"
+  },
+  {
+    "revision": "b3be9973598c4b3516e6f6c0338a32c9",
+    "url": "/portfolio/static/media/portfolio.b3be9973.png"
+  },
+  {
+    "revision": "0b11483c06ed3e1778ddd345320e6091",
+    "url": "/portfolio/static/media/rps.0b11483c.png"
+  },
+  {
+    "revision": "004573b4f9533a9b1a07669b03096da3",
+    "url": "/portfolio/static/media/todolist.004573b4.png"
+  },
+  {
+    "revision": "ab3ca9e2c66154ad7dcd8bd241a3b94d",
+    "url": "/portfolio/static/media/yummy-ice-cream.ab3ca9e2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1de2ab8f887161f5fb6022537a91489f.js"
+  "/portfolio/precache-manifest.9b32dc18a416f2e03c01c9bdec270c95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
